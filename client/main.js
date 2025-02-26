77e3b595130905f21f67aa9e05b724ef19dc5e64
mukuvi <jamesmngandu@gmail.com>
2025-02-26 22:47:48 +0300

    cafesynca

--- client/main.js
+++ client/main.js
@@ -1,9 +1,22 @@
-let id = (id) => document.getElementById("id");
-let classes = (classes) => document.getElementsByClassName("classes");
+let id = (id) => document.getElementById(id);
+let classes = (classes) => document.getElementsByClassName(classes);
 let username = id("username"),
   email = id("email'"),
   password = id("password"),
+  form = id("form"),
   errorMsg = classes("error"),
   successIcon = classes("success-icon"),
   failureIcon = classes("failure-icon");
-errorMsg[0].innerHTML = "hhhhhhh";
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (username.value === "") {
+    errorMsg[0].innerHTML = "username cannot be blank";
+  } else if (email.value === "") {
+    errorMsg[1].innerHTML = "email cannot be blank";
+  } else if (password.value === "") {
+    errorMsg[2].innerHTML = "password cannot be blank";
+  } else {
+    alert("done");
+  }
+});
