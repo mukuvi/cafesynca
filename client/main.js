473faa8f80c305dc74d7c9cd232d0afa4ec2e352
mukuvi <jamesmngandu@gmail.com>
2025-02-26 23:08:07 +0300

    cafesynca

--- client/main.js
+++ client/main.js
@@ -1,7 +1,7 @@
 let id = (id) => document.getElementById(id);
 let classes = (classes) => document.getElementsByClassName(classes);
 let username = id("username"),
-  email = id("email'"),
+  email = id("email"),
   password = id("password"),
   form = id("form"),
   errorMsg = classes("error"),
@@ -10,12 +10,19 @@ let username = id("username"),
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  if (username.value === "") {
-    errorMsg[0].innerHTML = "username cannot be blank";
-    failureIcon[0].style.opacity = "1";
-  } else {
-    errorMsg[0].innerHTML = "";
-    failureIcon[0].style.opacity = "0";
-    successIcon[0].style.opacity = "1";
-  }
+  engine(username, 0, "username cannot be blank");
+  engine(email, 1, "email cannot be blank");
+  engine(password, 2, "password cannot be blank");
 });
+
+let engine = (id, serial, message) => {
+  if (id.value === "") {
+    errorMsg[serial].innerHTML = message;
+    failureIcon[serial].style.opacity = "1";
+    successIcon[serial].style.opacity = "0";
+  } else {
+    errorMsg[serial].innerHTML = "";
+    failureIcon[serial].style.opacity = "0";
+    successIcon[serial].style.opacity = "1";
+  }
+};
