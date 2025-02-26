b1a59eea5d407c9d288e44bc7793744e6fb8925a
mukuvi <jamesmngandu@gmail.com>
2025-02-26 22:20:10 +0300

    cafesynca

--- client/main.js
+++ client/main.js
@@ -1,4 +1,6 @@
 let id = (id) => document.getElementById("id");
 let username = id("username"),
   email = id("email'"),
-  password = id("password");
+  password = id("password"),
+  erroMsg = document.getElementsByClassName("error");
+erroMsg = ["error", "error", "error"];
