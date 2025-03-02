cb480bb89d69e3a0bdb8ee7158d2d32e6650d3e8
mukuvi <jamesmngandu@gmail.com>
2025-03-02 17:53:00 +0300

    commit

--- v3/client/src/App.jsx
+++ v3/client/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
 
 function App() {
   return (
     <>
-      <div>h</div>
+      <div className="text-center">h</div>
     </>
   );
 }
