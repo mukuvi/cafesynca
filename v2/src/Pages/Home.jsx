71883ace48dc1fab0f09c198900c94c30adf781a
mukuvi <jamesmngandu@gmail.com>
2025-03-20 20:37:42 +0300

    mukuvi

--- v2/src/Pages/Home.jsx
+++ v2/src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import { Data } from "../components/Data";
 export const Home = () => {
   return (
     <>
-      <Data name="james" age="21" school="University of eldoret" />
+      <Data />
     </>
   );
 };
--- v2/src/components/Data.jsx
+++ v2/src/components/Data.jsx
@@ -1,9 +1,13 @@
-export const Data = ({ name, age, school }) => {
-  return (
-    <div>
-      <h1>{name}</h1>
-      <h1>{age}</h1>
-      <h1>{school}</h1>
-    </div>
-  );
+import myData from "../Data/myData";
+
+export const Data = () => {
+  const alldata = myData.map((data) => {
+    return (
+      <div key={data.name}>
+        <h1>{data.name}</h1> <h1>{data.age}</h1> <h1>{data.school}</h1>
+      </div>
+    );
+  });
+
+  return <div>{alldata}</div>;
 };
