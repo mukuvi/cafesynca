25d86bb00755b3c694839e04720415453097969b
mukuvi <jamesmngandu@gmail.com>
2025-03-20 22:04:59 +0300

    mukuvi

--- v2/src/components/Counter.jsx
+++ v2/src/components/Counter.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 export const Counter = () => {
+  const [count, setCount] = useState(0);
+  function handleIncrement() {
+    setCount(count + 1);
+  }
+  function handleDencrement() {
+    setCount(count - 1);
+  }
+
   return (
     <>
-      <p>Counter</p>
+      <p>Counter: {count}</p>
+      <button onClick={handleIncrement}>Count++</button>
+      <button onClick={handleDencrement}>Count--</button>
     </>
   );
 };
