3aed7a38d740de49c97661a001ecdc6a4807de3a
mukuvi <jamesmngandu@gmail.com>
2025-03-21 09:21:08 +0300

    cafesynca

--- v2/src/components/Form.jsx
+++ v2/src/components/Form.jsx
@@ -2,12 +2,6 @@ export const Form = () => {
   return (
     <>
       <p>forms</p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit
-        architecto quod omnis ipsum consectetur eaque dolorem corporis nulla,
-        alias doloremque distinctio molestias, dolor optio illo laborum
-        laboriosam eligendi. Magni, obcaecati?
-      </p>
     </>
   );
 };
