741d10dd02bac3c6b333132e85f88bb4f73d9408
mukuvi <jamesmngandu@gmail.com>
2025-03-21 09:20:56 +0300

    cafesynca

--- v2/src/Pages/Home.jsx
+++ v2/src/Pages/Home.jsx
@@ -1,9 +1,11 @@
 import { Counter } from "../components/Counter";
 import { Data } from "../components/Data";
+import { Form } from "../components/Form";
 
 export const Home = () => {
   return (
     <>
+      <Form />
       <Counter />
       <Data />
     </>
