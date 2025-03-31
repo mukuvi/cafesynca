dbab28ae5d73efe2ca6deb5c784f9d20c97bd5dc
mukuvi <jamesmngandu@gmail.com>
2025-04-01 00:55:32 +0300

    cafesynca

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,20 @@
 import "./App.css";
+import { Club } from "./Pages/Club";
 import { Home } from "./Pages/Home";
+import { NotFound } from "./Pages/NotFound";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <div class="bg-[#f9f6f0] ">
-        <Home />
+    <Router>
+      <div className="bg-[#f9f6f0]">
+        <Routes>
+          <Route path="/club" element={<Club />} />
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 }
 
